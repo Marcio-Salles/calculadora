b70f8d64cecafadf69860642c61bbaa228073008
Marcio-Salles <sallesmarcio1978@gmail.com>
2024-01-27 20:57:17 -0300

    calculadora

---
+++ src/app/marcio/Calculadora.java
@@ -0,0 +1,17 @@
+package app.marcio;
+
+import javax.swing.*;
+
+public class Calculadora extends JFrame {
+
+    // Criar um metodo construtor
+    public Calculadora() {
+    }
+
+    // Chamar o construtor
+
+    public static void main(String[] args) {
+
+        new Calculadora();
+    }
+}
